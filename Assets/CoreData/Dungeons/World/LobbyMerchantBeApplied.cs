c54701e803d6258c9aa5d57d580d60251bbce472
cukr2675 <cukr2675@gmail.com>
2024-02-09 22:08:16 +0900

    Update: LobbyMerchant sells Padleaf

--- Assets/CoreData/Dungeons/World/LobbyMerchantBeApplied.cs
+++ Assets/CoreData/Dungeons/World/LobbyMerchantBeApplied.cs
@@ -25,9 +25,10 @@ namespace Roguegard
             if (user == RogueDevice.Primary.Player)
             {
                 rogueMenu ??= new RogueMenu() { parent = this };
-                //RogueDevice.Primary.AddMenu(rogueMenu, user, null, RogueMethodArgument.Identity);
                 RogueDevice.Add(DeviceKw.AppendText, DeviceKw.StartTalk);
-                RogueDevice.Add(DeviceKw.AppendText, "商人「わたしは商人です でもまだ準備中です");
+                RogueDevice.Add(DeviceKw.AppendText, "商人「わたしは商人です でもまだ準備中です\n");
+                RogueDevice.Add(DeviceKw.AppendText, DeviceKw.EndTalk);
+                RogueDevice.Primary.AddMenu(rogueMenu, user, null, RogueMethodArgument.Identity);
                 return true;
             }
             else
@@ -42,15 +43,32 @@ namespace Roguegard
             return 0;
         }
 
-        private class RogueMenu : IModelsMenu
+        private class RogueMenu : IModelsMenu, IModelsMenuItemController
         {
             public LobbyMerchantBeApplied parent;
 
             public void OpenMenu(IModelsMenuRoot root, RogueObj self, RogueObj user, in RogueMethodArgument arg)
             {
                 var scroll = (IScrollModelsMenuView)root.Get(DeviceKw.MenuScroll);
-                scroll.OpenView(ChoicesModelsMenuItemController.Instance, parent._items, root, self, user, arg);
+                scroll.OpenView(this, parent._items, root, self, user, arg);
                 scroll.ShowExitButton(ExitModelsMenuChoice.Instance);
+            }
+
+            public string GetName(object model, IModelsMenuRoot root, RogueObj self, RogueObj user, in RogueMethodArgument arg)
+            {
+                return ((ScriptableStartingItem)model).Name;
+            }
+
+            public void Activate(object model, IModelsMenuRoot root, RogueObj self, RogueObj user, in RogueMethodArgument arg)
+            {
+                root.AddObject(DeviceKw.EnqueueSE, DeviceKw.Submit);
+                root.AddObject(DeviceKw.AppendText, DeviceKw.StartTalk);
+                root.AddObject(DeviceKw.AppendText, model);
+                root.AddObject(DeviceKw.AppendText, "を手に入れた\n");
+                root.AddObject(DeviceKw.AppendText, DeviceKw.EndTalk);
+
+                var startingItem = (ScriptableStartingItem)model;
+                startingItem.Option.CreateObj(startingItem, self, Vector2Int.zero, RogueRandom.Primary);
             }
         }
     }
