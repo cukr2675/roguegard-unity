54b2ff977950c8965c22afbe1e4f24252b0c24bb
cukr2675 <cukr2675@gmail.com>
2024-03-09 17:53:06 +0900

    Update EquipKw: IKeyword -> ISerializableKeyword

--- Assets/CoreData/ObjInfos/Keywords/EquipKw.cs
+++ Assets/CoreData/ObjInfos/Keywords/EquipKw.cs
@@ -8,50 +8,50 @@ namespace Roguegard
     {
         private static EquipKw instance;
 
-        [SerializeField] private KeywordData _accessory;
-        public static IKeyword Accessory => instance._accessory;
+        [SerializeField] private EquipKeywordData _accessory;
+        public static ISerializableKeyword Accessory => instance._accessory;
 
-        [SerializeField] private KeywordData _ammo;
-        public static IKeyword Ammo => instance._ammo;
+        [SerializeField] private EquipKeywordData _ammo;
+        public static ISerializableKeyword Ammo => instance._ammo;
 
-        [SerializeField] private KeywordData _bodyArmor;
-        public static IKeyword BodyArmor => instance._bodyArmor;
+        [SerializeField] private EquipKeywordData _bodyArmor;
+        public static ISerializableKeyword BodyArmor => instance._bodyArmor;
 
-        [SerializeField] private KeywordData _boots;
-        public static IKeyword Boots => instance._boots;
+        [SerializeField] private EquipKeywordData _boots;
+        public static ISerializableKeyword Boots => instance._boots;
 
-        [SerializeField] private KeywordData _bottoms;
-        public static IKeyword Bottoms => instance._bottoms;
+        [SerializeField] private EquipKeywordData _bottoms;
+        public static ISerializableKeyword Bottoms => instance._bottoms;
 
-        [SerializeField] private KeywordData _cloak;
-        public static IKeyword Cloak => instance._cloak;
+        [SerializeField] private EquipKeywordData _cloak;
+        public static ISerializableKeyword Cloak => instance._cloak;
 
-        [SerializeField] private KeywordData _faceMask;
-        public static IKeyword FaceMask => instance._faceMask;
+        [SerializeField] private EquipKeywordData _faceMask;
+        public static ISerializableKeyword FaceMask => instance._faceMask;
 
-        [SerializeField] private KeywordData _gloves;
-        public static IKeyword Gloves => instance._gloves;
+        [SerializeField] private EquipKeywordData _gloves;
+        public static ISerializableKeyword Gloves => instance._gloves;
 
-        [SerializeField] private KeywordData _headwear;
-        public static IKeyword Headwear => instance._headwear;
+        [SerializeField] private EquipKeywordData _headwear;
+        public static ISerializableKeyword Headwear => instance._headwear;
 
-        [SerializeField] private KeywordData _innerwear;
-        public static IKeyword Innerwear => instance._innerwear;
+        [SerializeField] private EquipKeywordData _innerwear;
+        public static ISerializableKeyword Innerwear => instance._innerwear;
 
-        [SerializeField] private KeywordData _lenses;
-        public static IKeyword Lenses => instance._lenses;
+        [SerializeField] private EquipKeywordData _lenses;
+        public static ISerializableKeyword Lenses => instance._lenses;
 
-        [SerializeField] private KeywordData _shield;
-        public static IKeyword Shield => instance._shield;
+        [SerializeField] private EquipKeywordData _shield;
+        public static ISerializableKeyword Shield => instance._shield;
 
-        [SerializeField] private KeywordData _socks;
-        public static IKeyword Socks => instance._socks;
+        [SerializeField] private EquipKeywordData _socks;
+        public static ISerializableKeyword Socks => instance._socks;
 
-        [SerializeField] private KeywordData _tops;
-        public static IKeyword Tops => instance._tops;
+        [SerializeField] private EquipKeywordData _tops;
+        public static ISerializableKeyword Tops => instance._tops;
 
-        [SerializeField] private KeywordData _weapon;
-        public static IKeyword Weapon => instance._weapon;
+        [SerializeField] private EquipKeywordData _weapon;
+        public static ISerializableKeyword Weapon => instance._weapon;
 
         public override IEnumerator LoadAsync()
         {
