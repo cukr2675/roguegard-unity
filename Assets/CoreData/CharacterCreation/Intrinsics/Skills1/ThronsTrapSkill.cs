02d7b3dd9a373cdfe7f68c65793bc6d98147a15f
cukr2675 <cukr2675@gmail.com>
2024-03-11 21:31:58 +0900

    Fix ThronsTrapSkill

--- Assets/CoreData/CharacterCreation/Intrinsics/Skills1/ThronsTrapSkill.cs
+++ Assets/CoreData/CharacterCreation/Intrinsics/Skills1/ThronsTrapSkill.cs
@@ -27,9 +27,7 @@ namespace Roguegard.CharacterCreation
 
             protected override bool Activate(RogueObj self, RogueObj user, float activationDepth, in RogueMethodArgument arg)
             {
-                if (!arg.TryGetTargetPosition(out var targetPosition) ||
-                    !RogueDirection.TryFromSign(targetPosition - self.Position, out var targetDirection)) return false;
-
+                var targetDirection = RogueMethodUtility.GetTargetDirection(self, arg);
                 self.Main.Stats.Direction = targetDirection;
                 var point0 = SpaceUtility.Raycast(
                     self.Location, self.Position, self.Main.Stats.Direction.Rotate(-1), 1, false, false, true, out _, out var pointHit0, out _);
