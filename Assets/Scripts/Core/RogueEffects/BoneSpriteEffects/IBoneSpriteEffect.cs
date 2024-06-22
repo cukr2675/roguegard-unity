4e7202081e76ce7091cdb71d2232e7ef2e648316
cukr2675 <cukr2675@gmail.com>
2024-06-22 15:37:52 +0900

    Update IBoneSpriteEffect: Add comment

--- Assets/Scripts/Core/RogueEffects/BoneSpriteEffects/IBoneSpriteEffect.cs
+++ Assets/Scripts/Core/RogueEffects/BoneSpriteEffects/IBoneSpriteEffect.cs
@@ -6,6 +6,11 @@ using SkeletalSprite;
 
 namespace Roguegard
 {
+    /// <summary>
+    /// ボーンを変化させるエフェクト。
+    /// 新しいボーンを追加するなど、ボーンの構成を変更する場合はこのエフェクトではなく <see cref="MainInfoSet"/> を使用する。
+    /// （同じボーンでも身長などによって異なる位置にしたほうが自然になる可能性があり、その場合は全体の構成を作り直すほうが適切であるため）
+    /// </summary>
     public interface IBoneSpriteEffect
     {
         /// <summary>
