21ad249c4143a993189b3123a02e6737dbb3e1d2
cukr2675 <cukr2675@gmail.com>
2024-06-23 22:23:42 +0900

    Add FireballSkill

---
+++ Assets/CoreData/CharacterCreation/Intrinsics/Skills2/FireballSkill.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Roguegard.Extensions;
+
+namespace Roguegard.CharacterCreation
+{
+    public class FireballSkill : MPSkillIntrinsicOptionScript
+    {
+        public override ISortedIntrinsic CreateSortedIntrinsic(
+            ScriptIntrinsicOption parent, IReadOnlyIntrinsic intrinsic, ICharacterCreationData characterCreationData, int lv)
+        {
+            return new SortedIntrinsic(parent, lv);
+        }
+
+        [Objforming.Formable]
+        private class SortedIntrinsic : MPSkillSortedIntrinsic<SortedIntrinsic>
+        {
+            public override IRogueMethodTarget Target => ForEnemyRogueMethodTarget.Instance;
+            public override IRogueMethodRange Range => LineOfSight10RogueMethodRange.Instance;
+            public override int RequiredMP => 2;
+
+            private SortedIntrinsic() : base(null, 0) { }
+
+            public SortedIntrinsic(ScriptIntrinsicOption parent, int lv) : base(parent, lv) { }
+
+            protected override bool Activate(RogueObj self, RogueObj user, float activationDepth, in RogueMethodArgument arg)
+            {
+                if (RaycastAssert.RequireTarget(LineOfSight10RogueMethodRange.Instance, self, arg, out var target)) return false;
+                if (MessageWorkListener.TryOpenHandler(self.Location, self.Position, out var h))
+                {
+                    using var handler = h;
+                    handler.AppendText(":ActivateSkillMsg::2").AppendText(self).AppendText(this).AppendText("\n");
+                    MainCharacterWorkUtility.TryAddSkill(self);
+                    MainCharacterWorkUtility.TryAddShot(self);
+                }
+
+                // 攻撃力(x2)ダメージの火属性攻撃。
+                using var damageValue = AffectableValue.Get();
+                StatsEffectedValues.GetATK(self, damageValue);
+                damageValue.MainValue += damageValue.BaseMainValue;
+                damageValue.SubValues[MainInfoKw.Skill] = 1f;
+                damageValue.SubValues[ElementKw.Fire] = 1f;
+                this.TryHurt(target, self, activationDepth, damageValue);
+                this.TryDefeat(target, self, activationDepth, damageValue);
+                return true;
+            }
+
+            public override int GetATK(RogueObj self, out bool additionalEffect)
+            {
+                // 攻撃力(x2)ダメージの攻撃。
+                using var damageValue = AffectableValue.Get();
+                StatsEffectedValues.GetATK(self, damageValue);
+                damageValue.MainValue += damageValue.BaseMainValue;
+
+                var hpDamage = Mathf.FloorToInt(damageValue.MainValue);
+                additionalEffect = false;
+                return hpDamage;
+            }
+        }
+    }
+}
