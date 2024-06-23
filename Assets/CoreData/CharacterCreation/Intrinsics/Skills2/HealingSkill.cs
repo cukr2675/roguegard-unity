4cbb939424f08d66d7c9dfd2f866ab0d8e4d7bfc
cukr2675 <cukr2675@gmail.com>
2024-06-23 22:25:20 +0900

    Add HeadlingSkill

---
+++ Assets/CoreData/CharacterCreation/Intrinsics/Skills2/HealingSkill.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Roguegard.Extensions;
+
+namespace Roguegard.CharacterCreation
+{
+    public class HealingSkill : MPSkillIntrinsicOptionScript
+    {
+        public override ISortedIntrinsic CreateSortedIntrinsic(
+            ScriptIntrinsicOption parent, IReadOnlyIntrinsic intrinsic, ICharacterCreationData characterCreationData, int lv)
+        {
+            return new SortedIntrinsic(parent, lv);
+        }
+
+        [Objforming.Formable]
+        private class SortedIntrinsic : MPSkillSortedIntrinsic<SortedIntrinsic>
+        {
+            public override IRogueMethodTarget Target => WoundedPartyMemberRogueMethodTarget.Instance;
+            public override IRogueMethodRange Range => UserRogueMethodRange.Instance;
+            public override int RequiredMP => 3;
+
+            private SortedIntrinsic() : base(null, 0) { }
+
+            public SortedIntrinsic(ScriptIntrinsicOption parent, int lv) : base(parent, lv) { }
+
+            protected override bool Activate(RogueObj self, RogueObj user, float activationDepth, in RogueMethodArgument arg)
+            {
+                if (RaycastAssert.RequireTarget(LineOfSight10RogueMethodRange.Instance, self, arg, out var target)) return false;
+                if (MessageWorkListener.TryOpenHandler(self.Location, self.Position, out var h))
+                {
+                    using var handler = h;
+                    handler.AppendText(":ActivateSkillMsg::2").AppendText(self).AppendText(this).AppendText("\n");
+                    MainCharacterWorkUtility.TryAddSkill(self);
+                    MainCharacterWorkUtility.TryAddShot(self);
+                }
+
+                // HPを5回復
+                using var healValue = AffectableValue.Get();
+                healValue.Initialize(5f);
+                healValue.SubValues[StdKw.Heal] = 1f;
+                this.TryHurt(target, self, activationDepth, healValue);
+                return true;
+            }
+
+            public override int GetATK(RogueObj self, out bool additionalEffect)
+            {
+                additionalEffect = true;
+                return 0;
+            }
+        }
+    }
+}
