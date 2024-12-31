b9a4da84ead5c3e81d05585f8afc4c23cab11cbb
cukr2675 <cukr2675@gmail.com>
2024-12-31 16:51:11 +0900

    Add Rename item feature

--- Assets/Scripts/Unity/StandardRogueDevice/Menu/Data/ObjCommandMenu.cs
+++ Assets/Scripts/Unity/StandardRogueDevice/Menu/Data/ObjCommandMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using ListingMF;
 using Roguegard;
 using Roguegard.Device;
+using Roguegard.Extensions;
 
 namespace RoguegardUnity
 {
@@ -17,9 +18,11 @@ namespace RoguegardUnity
         private readonly List<ISelectOption> selectOptions;
         private readonly SummaryMenuScreen summaryMenuScreen = new();
         private readonly DetailsMenuScreen detailsMenuScreen = new();
+        private readonly RenameDialog renameDialog = new();
 
         public ISelectOption Summary { get; }
         public ISelectOption Details { get; }
+        public ISelectOption Rename { get; }
 
         private readonly CommandListViewTemplate<ISelectOption, MMgr, MArg> view = new()
         {
@@ -40,6 +43,10 @@ namespace RoguegardUnity
             {
                 manager.PushMenuScreen(detailsMenuScreen, arg);
             });
+            Rename = SelectOption.Create<MMgr, MArg>("名前変更", (manager, arg) =>
+            {
+                manager.PushMenuScreen(renameDialog, arg);
+            });
         }
 
         public override void OpenScreen(in MMgr manager, in MArg arg)
@@ -52,6 +59,7 @@ namespace RoguegardUnity
                 selectOptions.Add(command.SelectOption);
             }
             selectOptions.Add(Details);
+            selectOptions.Add(Rename);
             selectOptions.Add(BackSelectOption.Instance);
 
             view.Title = StandardRogueDeviceUtility.GetCaption(tool.Main.InfoSet);
@@ -122,6 +130,62 @@ namespace RoguegardUnity
                 view.ShowTemplate(details ?? "", manager, arg)
                     ?
                     .Build();
+            }
+        }
+
+        private class RenameDialog : RogueMenuScreen
+        {
+            private string newName;
+
+            private readonly DialogViewTemplate<MMgr, MArg> view = new()
+            {
+                BackAnchorSubViewName = null,
+            };
+
+            public override bool IsIncremental => true;
+
+            public override void OpenScreen(in MMgr manager, in MArg arg)
+            {
+                view.ShowTemplate("", manager, arg)
+                    ?
+                    .Append(InputFieldViewWidget.CreateOption<MMgr, MArg>(
+                        (manager, arg) =>
+                        {
+                            var obj = arg.Arg.Tool ?? arg.Arg.TargetObj;
+                            return newName = NamingEffect.Get(obj)?.Naming;
+                        },
+                        (manager, arg, value) =>
+                        {
+                            return newName = value;
+                        })
+                    )
+
+                    .Append(new object[]
+                    {
+                        SelectOption.Create<MMgr, MArg>(":Rename", (manager, arg) =>
+                        {
+                            var obj = arg.Arg.Tool ?? arg.Arg.TargetObj;
+                            default(IActiveRogueMethodCaller).Affect(obj, 1f, NamingEffect.Callback);
+                            if (string.IsNullOrWhiteSpace(newName))
+                            {
+                                BaseStatusEffect.Close<NamingEffect>(obj);
+                            }
+                            else
+                            {
+                                NamingEffect.Get(obj).Naming = newName;
+                            }
+                            manager.Back(2);
+                            manager.Reopen();
+                        }),
+                        BackSelectOption.Instance
+                    })
+
+                    .Build();
+            }
+
+            public override void CloseScreen(MMgr manager, bool back)
+            {
+                view.HideTemplate(manager, back);
             }
         }
     }
