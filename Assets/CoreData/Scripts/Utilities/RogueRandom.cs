0f86d0e9af269038016a4ac35b628b2e92d6bd1b
cukr2675 <cukr2675@gmail.com>
2025-07-20 17:41:32 +0900

    Update RogueRandom

--- Assets/CoreData/Scripts/Utilities/RogueRandom.cs
+++ Assets/CoreData/Scripts/Utilities/RogueRandom.cs
@@ -1,7 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Roguegard
 {
     [Objforming.Formable]
@@ -9,10 +5,12 @@ namespace Roguegard
     {
         [System.NonSerialized] private System.Random random;
 
-        private readonly int seed;
+        private int seed;
         private int position;
 
         public static IRogueRandom Primary { get; set; }
+
+        private const int maxPosition = 100000;
 
         public RogueRandom()
         {
@@ -31,12 +29,16 @@ namespace Roguegard
         {
             if (random == null) { Initialize(); }
 
+            IncrementPosition();
+
             return random.Next(minInclusive, maxExclusive);
         }
 
         public float NextFloat(float min, float max)
         {
             if (random == null) { Initialize(); }
+
+            IncrementPosition();
 
             var size = (double)max - min; // float.MaxValue を超えないように double にキャストしてから計算する
             return (float)(min + random.NextDouble() * size);
@@ -45,9 +47,22 @@ namespace Roguegard
         private void Initialize()
         {
             random = new System.Random(seed);
-            for (int i = 0; i < position; i++)
+            for (int i = 0; i < position; i++) // シリアル化前の乱数位置を修復する
             {
                 random.Next();
+            }
+        }
+
+        private void IncrementPosition()
+        {
+            if (position >= maxPosition) // 一定回数以上は乱数位置の修復に時間がかかることを考慮して別シードで最初からにする
+            {
+                seed = random.Next();
+                position = 0;
+            }
+            else
+            {
+                position++;
             }
         }
     }
