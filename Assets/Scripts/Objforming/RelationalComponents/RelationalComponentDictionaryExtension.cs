22ef2c8c64b277e771dd6924fe5cb7add6f411ab
cukr2675 <cukr2675@gmail.com>
2025-08-11 10:18:43 +0900

    Rename RelationalComponentTableExtension -> RelationalComponentDictionaryExtension

--- Assets/Scripts/Objforming/RelationalComponents/RelationalComponentDictionaryExtension.cs
+++ Assets/Scripts/Objforming/RelationalComponents/RelationalComponentDictionaryExtension.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Objforming
 {
-    public static class RelationalComponentTableExtension
+    public static class RelationalComponentDictionaryExtension
     {
         /// <summary>
         /// <see cref="IRelationalComponent.Overrides(IRelationalComponent)"/> が true を返すとき上書きで追加するメソッド
