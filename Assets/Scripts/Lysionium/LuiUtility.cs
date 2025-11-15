a0082ee1d0fdf8e521370afd118e41069a0fdf99
cukr2675 <cukr2675@gmail.com>
2025-11-16 08:23:36 +0900

    Update Lysionium.LuiUtility

--- Assets/Scripts/Lysionium/LuiUtility.cs
+++ Assets/Scripts/Lysionium/LuiUtility.cs
@@ -30,11 +30,17 @@ namespace Lysionium
             if (TryGetComponentInParent<PlayerInput>(transform, out var playerInput) && playerInput.uiInputModule)
             {
                 // PlayerInput が存在するかつ uiInputModule が設定されている場合は同一オブジェクトの EventSystem を使用する（MultiplayerEventSystem 対策）
-                return playerInput.uiInputModule.GetComponent<EventSystem>();
+                if (!playerInput.uiInputModule.TryGetComponent<EventSystem>(out var result))
+                    throw new System.InvalidOperationException($"{playerInput}.uiInputModule の {nameof(EventSystem)} が見つかりません。");
+
+                return result;
             }
             else
             {
-                return EventSystem.current;
+                var result = EventSystem.current;
+                if (result == null) throw new System.InvalidOperationException($"{nameof(EventSystem)} が見つかりません。");
+
+                return result;
             }
         }
 
