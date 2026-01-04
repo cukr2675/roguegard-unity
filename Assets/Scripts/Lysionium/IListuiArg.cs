62e58ae14839e0889064351a122c15c75e281b01
cukr2675 <cukr2675@gmail.com>
2026-01-04 21:06:03 +0900

    Refact Lysionium: Add comment (IListuiArg)

--- Assets/Scripts/Lysionium/IListuiArg.cs
+++ Assets/Scripts/Lysionium/IListuiArg.cs
@@ -1,5 +1,11 @@
 namespace Lysionium
 {
+    // 命名メモ: IListuiParameter, IListuiParam にして TParam にすると型引数名が長すぎる。 TParm, TPrm は parameter の略として一般的ではない
+    // 予約語の 'params' と被りやすいのもデメリット
+
+    /// <summary>
+    /// <see cref="IListuiManager"/> の各画面に渡す引数となるインターフェース
+    /// </summary>
     public interface IListuiArg
     {
         void CopyTo(ref IListuiArg dest);
